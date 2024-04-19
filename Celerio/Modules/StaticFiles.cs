00aad01226333cd14401580288c254769cd049b3
Oxule <ribb2017@mail.ru>
2024-04-19 23:56:36 +0300

    Static Files

---
+++ Celerio/Modules/StaticFiles.cs
@@ -0,0 +1,45 @@
+﻿namespace Celerio;
+
+public class StaticFiles : IAfterRequest
+{
+    public struct StaticFile
+    {
+        public string Path;
+        public string ContentType;
+
+        public StaticFile(string path, string contentType)
+        {
+            Path = path;
+            ContentType = contentType;
+        }
+    }
+
+    public Dictionary<string, StaticFile> Files { get; set; } = new Dictionary<string, StaticFile>();
+    
+
+    public StaticFiles(Dictionary<string, StaticFile> files)
+    {
+        Files = files;
+    }
+    
+    public StaticFiles()
+    {
+    }
+    
+    public HttpResponse? AfterRequestHandler(HttpRequest request, Pipeline pipeline)
+    {
+        if (request.Method != "GET") 
+            return null;
+        
+        foreach (var file in Files)
+        {
+            Console.WriteLine($"{file.Key} - {request.URI}");
+            if (file.Key != request.URI)
+                continue;
+
+            return HttpResponse.File(new FileStream(file.Value.Path, FileMode.Open, FileAccess.Read), file.Value.ContentType);
+        }
+
+        return null;
+    }
+}
