42812c450cb940558ccdc8f4f11b24e47cf80792
Oxule <ribb2017@mail.ru>
2024-06-16 22:45:44 +0300

    Moved Swagger To Another Project

--- CelerioSamples/Program.cs
+++ CelerioSamples/Program.cs
@@ -2,8 +2,5 @@
 
 var pipeline = new Pipeline();
 
-pipeline.Modules.Add(new SwaggerGen());
-pipeline.Modules.Add(new SwaggerUI());
-
 Server server = new Server(pipeline);
 await server.StartListening(5000);
