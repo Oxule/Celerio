d61ff368138ac817ae0a180af75fc4c7693332c8
Oxule <ribb2017@mail.ru>
2024-09-18 14:46:53 +0300

    Response Attribute Removed

--- Celerio/Attributes.cs
+++ Celerio/Attributes.cs
@@ -1,20 +1,5 @@
 ﻿namespace Celerio;
 
-[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
-public class Response : Attribute
-{
-    public Type? Type { get; set; }
-    public int StatusCode { get; set; }
-    public string Description { get; set; }
-
-    public Response(int statusCode, string description, Type? type = null)
-    {
-        Type = type;
-        StatusCode = statusCode;
-        Description = description;
-    }
-} 
-
 [AttributeUsage(AttributeTargets.Method, Inherited = false)]
 public class Route : Attribute
 {
